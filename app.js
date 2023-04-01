a3e2a1d4c1c1ac2ca1593bc9504e9443571fe65a
Mariano Orona <maoaltacba@gmail.com>
2023-04-01 12:18:34 -0300

    developing paginate

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ import express from "express";
 import apiRouter from "./routers/api/index.js";
 import viewsRouter from "./routers/views/index.js";
 import { init } from "./dao/db/mongodb.js";
+import ProductModel from "./dao/models/product.js"
 
 
-
-init();
+ await init();
 const app = express();
+
+app.get("/", async (req, res) =>{
+    const options ={
+        limit: 10,
+        page: 1
+    }
+    const result = await ProductModel.paginate({}, options)
+    res.json(result)
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
